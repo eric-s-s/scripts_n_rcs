54fb98ab3c928e4f80a6e4754c4e5422b6774ebb
Eric Shaw <eric.shaw@datarobot.com>
2022-11-14 16:52:56 -0500

    updated

--- dr-repos.sh
+++ dr-repos.sh
@@ -1,9 +1,14 @@
 git clone git@github.com:datarobot/adipose.git
+git clone git@github.com:datarobot/admin-guide.git
 git clone git@github.com:datarobot/cicada.git
+git clone git@github.com:datarobot/cloud-docs.git
 git clone git@github.com:datarobot/coding-interview-questions.git
 git clone git@github.com:datarobot/custom-task-stackbot.git
+git clone git@github.com:datarobot/data-prep-devops.git
 git clone git@github.com:datarobot/datarobot-user-models.git
 git clone git@github.com:datarobot/data-science-scripts.git
+git clone git@github.com:datarobot/drcajenkins.git
+git clone git@github.com:datarobot/drlayout.git
 git clone git@github.com:datarobot/dr-usertool.git
 git clone git@github.com:datarobot/jenkins-jobs.git
 git clone git@github.com:datarobot/mldev-observability.git
@@ -12,3 +17,4 @@ git clone git@github.com:datarobot/mlops_infrastructure.git
 git clone git@github.com:datarobot/mlops-scripts.git
 git clone git@github.com:datarobot/public_api_client.git
 git clone git@github.com:datarobot/stackbot.git
+git clone git@github.com:datarobot/stackbot_blueprints.git
